12d8a45a077acee800c8f6c13fd53bb9c83e42b0
Richard Brown <flukefan@googlemail.com>
2015-04-14 14:40:25 +0100

    Added drop/create of test database.

--- Database.Tests/Migrations/TestMigrations.cs
+++ Database.Tests/Migrations/TestMigrations.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 using NUnit.Framework;
 
 namespace LucidDotNet.Database.Tests.Migrations
@@ -6,19 +7,43 @@ namespace LucidDotNet.Database.Tests.Migrations
     [TestFixture]
     public class TestMigrations
     {
+        private const string DropDb = @"
+            IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Lucid')
+            BEGIN
+                ALTER DATABASE Lucid
+                    SET SINGLE_USER
+                    WITH ROLLBACK IMMEDIATE
+
+                DROP DATABASE Lucid
+            END";
+
+        private const string CreateDb = "Create Database Lucid";
+
         [Test]
-        public void Migrations_CanUpgradeTablesContaingData()
+        public void Migrations_Run_UpgradesDatabase()
         {
             DropAndCreateBlankDb();
 
             Assert.Fail("got to here");
         }
 
+        private void ExecuteNonQuery(IDbConnection connection, string sql)
+        {
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+        }
+
         private void DropAndCreateBlankDb()
         {
             var environment = BuildEnvironment.Load();
-            var masterDb = new SqlConnection(environment.MasterConnection);
-            masterDb.Open();
+
+            using (var masterDb = new SqlConnection(environment.MasterConnection))
+            {
+                masterDb.Open();
+                ExecuteNonQuery(masterDb, DropDb);
+                ExecuteNonQuery(masterDb, CreateDb);
+            }
         }
     }
 }
