b6f556877e290b65c7c7ed23af6359cfffdb8a4e
Richard Brown <flukefan@googlemail.com>
2015-06-29 16:15:11 +0100

    Added domain and domain.test assemblies to build.

---
+++ Domain.Tests/Orgs/UserTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Lucid.Domain.Tests.Orgs
+{
+    [TestFixture]
+    public class UserTest
+    {
+        [Test]
+        public void Login_WhenDoesNotExist_IsCreated()
+        {
+            Assert.Fail("Login_WhenDoesNotExist_IsCreated not written yet");
+        }
+
+    }
+}
