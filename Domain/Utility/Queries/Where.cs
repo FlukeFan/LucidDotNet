d51c5605db2f975b29f1a70cbd6cf6a7b9882a55
Richard Brown <flukefan@googlemail.com>
2015-07-03 14:19:39 +0100

    Separated WhereStringEqual into its own file.

--- Domain/Utility/Queries/Where.cs
+++ Domain/Utility/Queries/Where.cs
@@ -26,24 +26,4 @@ namespace Lucid.Domain.Utility.Queries
             return new WhereStringEqual<T>(property, value);
         }
     }
-
-    public class WhereStringEqual<T> : Where<T>
-    {
-        private Func<T, string> _accessor;
-
-        public string  Value { get; private set; }
-
-        public WhereStringEqual(Expression<Func<T, string>> property, string value)
-            : base(property.Body)
-        {
-            _accessor = property.Compile();
-            Value = value;
-        }
-
-        public override bool Satisfies(T entity)
-        {
-            var entityValue = _accessor(entity);
-            return entityValue == Value;
-        }
-    }
 }
