90b9c669eedb803b3b495b1acd7d18ec9f8ae864
Richard Brown <flukefan@googlemail.com>
2015-09-08 22:24:53 +0100

    Added first MVC response.

---
+++ Web/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+using Lucid.Web.Startup;
+
+namespace Lucid.Web
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
