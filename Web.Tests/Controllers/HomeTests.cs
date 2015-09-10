de54143194dabe3729fd46c22bd02c328874cb07
Richard Brown <flukefan@googlemail.com>
2015-09-10 22:13:02 +0100

    Added place-holder test.

---
+++ Web.Tests/Controllers/HomeTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Web.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeTests
+    {
+        [Test]
+        public void A_Test()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
