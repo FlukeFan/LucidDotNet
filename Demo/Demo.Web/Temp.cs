6472a8fb064e923f6d48f8b2df8a0203a325ea0b
Richard Brown <flukefan@googlemail.com>
2015-10-04 15:06:13 +0100

    Added auto package restore to Demo (using packages generator for Lucid)

---
+++ Demo/Demo.Web/Temp.cs
@@ -0,0 +1,15 @@
+﻿using Lucid.Infrastructure.NHibernate;
+
+namespace Lucid.Demo.Web
+{
+    /// <summary>
+    /// Temporary class just to start compilation
+    /// </summary>
+    public static class Temp
+    {
+        public static void NotCalled()
+        {
+            Mapping.CreateMappings();
+        }
+    }
+}
