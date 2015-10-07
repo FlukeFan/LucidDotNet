ff2de2e1cb098cb3f00ccb34eacc7f883e0f1df6
Richard Brown <flukefan@googlemail.com>
2015-10-07 11:19:03 +0100

    Added project for Lucid.Domain

---
+++ Lucid.Domain.Tests/EmptyTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Lucid.Domain.Tests
+{
+    [TestFixture]
+    public class EmptyTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Ignore("placeholder test");
+        }
+    }
+}
