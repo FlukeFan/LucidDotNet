b6df2dac8e69f4700d04fbd1d35e4945f4ba5312
Richard Brown <flukefan@googlemail.com>
2015-10-13 18:04:48 +0100

    Added (missing) builder tests

--- Lucid.Domain.Testing/Builder.cs
+++ Lucid.Domain.Testing/Builder.cs
@@ -10,12 +10,22 @@ namespace Lucid.Domain.Testing
         ///  Utility method to create objects with protected constructor
         ///  e.g., Apple apple = ObjectBuilder.Create&lt;Apple&gt;();
         /// </summary>
-        public static T Create<T>()
+        public static T Create<T>(bool allowPublicConstructor = false)
         {
-            var constructors = typeof(T).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            var flags = allowPublicConstructor
+                ? BindingFlags.Instance | BindingFlags.Public
+                : BindingFlags.Instance | BindingFlags.NonPublic;
+
+            var constructors = typeof(T).GetConstructors(flags);
 
             if ((constructors == null) || (constructors.Length == 0))
-                throw new System.Exception("Couldn't find non-public default constructor on type " + typeof(T).Name);
+            {
+                var message = allowPublicConstructor
+                    ? "Couldn't find default constructor on type " + typeof(T).Name
+                    : "Couldn't find non-public default constructor on type " + typeof(T).Name;
+
+                throw new System.Exception(message);
+            }
 
             return (T)constructors[0].Invoke(null);
         }
@@ -39,27 +49,29 @@ namespace Lucid.Domain.Testing
 
     public class Builder<T> : Builder
     {
-        protected T _instance;
+        protected T     _instance;
+        protected bool  _allowPublicMutators;
 
         public Builder() : this(Create<T>()) { }
 
-        public Builder(T instance)
+        public Builder(T instance, bool allowPublicMutators = false)
         {
             if (instance == null)
                 throw new ArgumentNullException("Cannot have null instance when constructing " + GetType());
 
             _instance = instance;
+            _allowPublicMutators = allowPublicMutators;
         }
 
         public Builder<T> With<U>(Expression<Func<T, U>> propertyFunction, U value)
         {
             PropertyInfo propertyInfo = GetPropertyInfo(propertyFunction.Body);
 
-            if (propertyInfo.GetSetMethod() != null)
-                throw new System.Exception("Property '" + propertyInfo.Name + "' is not protected on " + _instance.ToString());
+            if (!_allowPublicMutators && propertyInfo.GetSetMethod() != null)
+                throw new System.Exception("Property '" + propertyInfo.Name + "' is not protected on " + _instance.GetType());
 
             if (!propertyInfo.CanWrite)
-                throw new System.Exception("Property '" + propertyInfo.Name + "' does not have a mutator on " + _instance.ToString());
+                throw new System.Exception("Property '" + propertyInfo.Name + "' does not have a mutator on " + _instance.GetType());
 
             propertyInfo.SetValue(_instance, value, null);
             return this;
