a9b80828afbffd8e3674c5d513d12e817f42dcb3
Richard Brown <flukefan@googlemail.com>
2015-10-14 22:11:51 +0100

    Added tests for ConsistencyInspector

--- Lucid.Domain.Testing/ConsistencyInspector.cs
+++ Lucid.Domain.Testing/ConsistencyInspector.cs
@@ -8,6 +8,13 @@ namespace Lucid.Domain.Testing
     {
         public static readonly DateTime MinSqlServerDateTime = new DateTime(1753, 1, 1, 0, 0, 0);
 
+        private bool _isMsSql;
+
+        public ConsistencyInspector(bool isMsSql = true)
+        {
+            _isMsSql = isMsSql;
+        }
+
         public virtual void BeforeSave(object entity)
         {
             var type = entity.GetType();
@@ -15,12 +22,19 @@ namespace Lucid.Domain.Testing
 
             foreach (var property in properties)
                 CheckProperty(entity, property);
-
-            CustomInspections.Verify(type, entity, this);
         }
 
         protected virtual void CheckProperty(object entity, PropertyInfo property)
         {
+            var type = property.PropertyType;
+
+            if (type == typeof(DateTime) && _isMsSql)
+                CheckMsSqlDateTime((DateTime)property.GetValue(entity), property.Name);
+        }
+
+        public void CheckMsSqlDateTime(Expression<Func<DateTime>> property)
+        {
+            CheckMsSqlDateTime(property.Compile().Invoke(), Builder.GetPropertyName(property.Body));
         }
 
         public void CheckMsSqlDateTime(DateTime dateTime, string propertyName)
@@ -29,15 +43,28 @@ namespace Lucid.Domain.Testing
                 throw new Exception(string.Format("DateTime property {0} with value {1} cannot be stored in SQL Server", propertyName, dateTime));
         }
 
-        public void CheckNotNull(Expression<Func<string>> property)
+        public void CheckNotNull(Expression<Func<object>> property)
         {
             CheckNotNull(property.Compile().Invoke(), Builder.GetPropertyName(property.Body));
         }
 
-        public void CheckNotNull(string value, string propertyName)
+        public void CheckNotNull(object value, string propertyName)
         {
             if (value == null)
-                throw new Exception(string.Format("string property {0} cannot be null", propertyName));
+                throw new Exception(string.Format("property {0} cannot be null", propertyName));
+        }
+
+        public void CheckNotNullOrEmpty(Expression<Func<string>> property)
+        {
+            CheckNotNullOrEmpty(property.Compile().Invoke(), Builder.GetPropertyName(property.Body));
+        }
+
+        public void CheckNotNullOrEmpty(string value, string propertyName)
+        {
+            CheckNotNull(value, propertyName);
+
+            if (value == string.Empty)
+                throw new Exception(string.Format("string property {0} cannot be empty", propertyName));
         }
     }
 }
---
+++ Lucid.Domain.Tests/Testing/ConsistencyInspectorTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using FluentAssertions;
+using Lucid.Domain.Testing;
+using NUnit.Framework;
+
+namespace Lucid.Domain.Tests.Testing
+{
+    [TestFixture]
+    public class ConsistencyInspectorTests
+    {
+        public class FakeEntity
+        {
+            public object   Object      { get; set; }
+            public DateTime DateTime    { get; set; }
+            public string   String      { get; set; }
+        }
+
+        [Test]
+        public void WhenMsSql_DateTimeIsValidated()
+        {
+            var inspector = new ConsistencyInspector();
+
+            Assert.Throws<Exception>(() => inspector.BeforeSave(new FakeEntity { DateTime = DateTime.MinValue }));
+
+            Assert.DoesNotThrow(() => inspector.BeforeSave(new FakeEntity { DateTime = new DateTime(2008, 07, 06) }));
+            Assert.DoesNotThrow(() => inspector.BeforeSave(new FakeEntity { DateTime = DateTime.MaxValue }));
+        }
+
+        [Test]
+        public void WhenNotMsSql_DateTimeIsIgnored()
+        {
+            var inspector = new ConsistencyInspector(isMsSql: false);
+
+            Assert.DoesNotThrow(() => inspector.BeforeSave(new FakeEntity { DateTime = DateTime.MinValue }));
+            Assert.DoesNotThrow(() => inspector.BeforeSave(new FakeEntity { DateTime = new DateTime(2008, 07, 06) }));
+            Assert.DoesNotThrow(() => inspector.BeforeSave(new FakeEntity { DateTime = DateTime.MaxValue }));
+        }
+
+        [Test]
+        public void Check_AllPropertiesValid()
+        {
+            var inspector = new ConsistencyInspector();
+            var entity = new FakeEntity
+            {
+                Object = new object(),
+                DateTime = new DateTime(2008, 07, 06),
+                String = "not null",
+            };
+
+            Assert.DoesNotThrow(() => inspector.CheckNotNull(() => entity.Object));
+            Assert.DoesNotThrow(() => inspector.CheckMsSqlDateTime(() => entity.DateTime));
+            Assert.DoesNotThrow(() => inspector.CheckNotNullOrEmpty(() => entity.String));
+        }
+
+        [Test]
+        public void CheckNotNull_ThrowsWhenNull()
+        {
+            var inspector = new ConsistencyInspector();
+            var entity = new FakeEntity { Object = null };
+
+            var e = Assert.Throws<Exception>(() => inspector.CheckNotNull(() => entity.Object));
+
+            e.Message.Should().Contain("Object cannot be null");
+        }
+
+        [Test]
+        public void CheckNotNullOrEmpty_Throws()
+        {
+            var inspector = new ConsistencyInspector();
+            var entity = new FakeEntity { String = null };
+
+            var e = Assert.Throws<Exception>(() => inspector.CheckNotNullOrEmpty(() => entity.String));
+
+            e.Message.Should().Contain("String cannot be null");
+        }
+
+        [Test]
+        public void CheckNotNullOrEmpty_ThrowsWhenEmpty()
+        {
+            var inspector = new ConsistencyInspector();
+            var entity = new FakeEntity { String = "" };
+
+            var e = Assert.Throws<Exception>(() => inspector.CheckNotNullOrEmpty(() => entity.String));
+
+            e.Message.Should().Contain("String cannot be empty");
+        }
+    }
+}
