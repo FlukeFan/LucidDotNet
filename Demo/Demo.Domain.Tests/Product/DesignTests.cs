86f5a4f6441625bff377ff2b511313b38b89b724
Richard Brown <flukefan@googlemail.com>
2015-10-21 10:10:35 +0100

    Added class for Design of a gadget.

---
+++ Demo/Demo.Domain.Tests/Product/DesignTests.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Domain.Product;
+using Demo.Domain.Tests.Utility;
+using Lucid.Domain.Testing;
+using NUnit.Framework;
+
+namespace Demo.Domain.Tests.Product
+{
+    public class DesignBuilder : Builder<Design>
+    {
+        static DesignBuilder()
+        {
+            DemoCustomInspections.Add<Design>((validator, entity) =>
+            {
+                validator.CheckNotNull(() => entity.Name);
+            });
+        }
+
+        public DesignBuilder()
+        {
+            With(u => u.Name, "test design 1");
+        }
+    }
+
+    [TestFixture]
+    public class DesignTests : DomainTest
+    {
+        [Test]
+        public void Construct()
+        {
+            new DesignBuilder().Save();
+        }
+    }
+}
---
+++ Demo/Demo.Domain/Product/Design.cs
@@ -0,0 +1,11 @@
+﻿using Demo.Domain.Utility;
+
+namespace Demo.Domain.Product
+{
+    public class Design : DemoEntity
+    {
+        protected Design() { }
+
+        public string Name { get; protected set; }
+    }
+}
