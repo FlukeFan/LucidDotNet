8f43e04a9b5529ae068f99bf39196212ae9df831
Richard Brown <flukefan@googlemail.com>
2015-11-22 15:45:29 +0000

    Added routes for each of F1, F2, F3

---
+++ Lucid.Web.Tests/StubApp/App/F2Tests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Lucid.Web.Tests.StubApp.Utility;
+using NUnit.Framework;
+
+namespace Lucid.Web.Tests.StubApp.App
+{
+    [TestFixture]
+    public class F2Tests : StubAppTest
+    {
+        [Test]
+        public void Index_Get()
+        {
+            StubApp.Test(http =>
+            {
+                string response = http.Get("/f2");
+
+                response.Should().Contain("F2/Index");
+            });
+        }
+    }
+}
---
+++ Lucid.Web.Tests/StubApp/App/F3Tests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Lucid.Web.Tests.StubApp.Utility;
+using NUnit.Framework;
+
+namespace Lucid.Web.Tests.StubApp.App
+{
+    [TestFixture]
+    public class F3Tests : StubAppTest
+    {
+        [Test]
+        public void Index_Get()
+        {
+            StubApp.Test(http =>
+            {
+                string response = http.Get("/f3");
+
+                response.Should().Contain("F3/Index");
+            });
+        }
+    }
+}
