897ffadae13be2bc0caca2e9bae3760ae6c08c2d
Richard Brown <flukefan@googlemail.com>
2015-11-25 11:00:53 +0000

    Added ability to output to host console window

---
+++ Lucid.Web.Testing/Hosting/ConsoleWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lucid.Web.Testing.Hosting
+{
+    public class ConsoleWriter : MarshalByRefObject
+    {
+        public void Write(object o)
+        {
+            Console.Write(o);
+        }
+
+        public void WriteLine(object o)
+        {
+            Console.WriteLine(o);
+        }
+    }
+}
--- Lucid.Web.Testing/Http/SimulatedHttpClient.cs
+++ Lucid.Web.Testing/Http/SimulatedHttpClient.cs
@@ -2,12 +2,20 @@
 using System.IO;
 using System.Web;
 using System.Web.Hosting;
+using Lucid.Web.Testing.Hosting;
 
 namespace Lucid.Web.Testing.Http
 {
     [Serializable]
     public class SimulatedHttpClient
     {
+        public ConsoleWriter ConsoleWriter { get; protected set; }
+
+        public SimulatedHttpClient()
+        {
+            ConsoleWriter = new ConsoleWriter();
+        }
+
         public string Get(string url)
         {
             url = url.TrimStart('~', '/');
