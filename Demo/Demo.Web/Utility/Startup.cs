25292e6b1e97ef6902a595517a964b75ef14b6bd
Richard Brown <flukefan@googlemail.com>
2015-12-19 15:44:37 +0000

    Tidied up retrieval of connection string.

--- Demo/Demo.Web/Utility/Startup.cs
+++ Demo/Demo.Web/Utility/Startup.cs
@@ -12,24 +12,34 @@ namespace Demo.Web.Utility
     {
         public virtual void InitRepository()
         {
-            var connectionString = ConfigurationManager.AppSettings["connectionString"];
-
-            var webFolder = HostingEnvironment.MapPath("~");
-            var configOverrideFile = Path.Combine(webFolder, @"..\BuildEnvironment.json");
-
-            if (File.Exists(configOverrideFile))
-            {
-                var json = File.ReadAllText(configOverrideFile);
-                var values = JsonConvert.DeserializeObject<IDictionary<string, string>>(json);
-
-                var key = "DemoConnection";
-
-                if (values.ContainsKey(key))
-                    connectionString = values[key];
-            }
+            var connectionString = DevConnectionStringOverride() ?? WebConfigConnnectionString();
 
             // TODO verify NH not hitting DB for reserved words
             DemoNhRepository.Init(connectionString, typeof(DemoEntity));
+        }
+
+        private string WebConfigConnnectionString()
+        {
+            return ConfigurationManager.AppSettings["connectionString"];
+        }
+
+        private string DevConnectionStringOverride()
+        {
+            var webFolder = HostingEnvironment.MapPath("~");
+            var configOverrideFile = Path.Combine(webFolder, @"..\BuildEnvironment.json");
+
+            if (!File.Exists(configOverrideFile))
+                return null;
+
+            var json = File.ReadAllText(configOverrideFile);
+            var values = JsonConvert.DeserializeObject<IDictionary<string, string>>(json);
+
+            var key = "DemoConnection";
+
+            if (!values.ContainsKey(key))
+                return null;
+
+            return values[key];
         }
     }
 }
