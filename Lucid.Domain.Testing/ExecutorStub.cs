1a2238194346f291d230bd888140c47ec62d4fb1
Richard Brown <flukefan@googlemail.com>
2016-01-08 14:50:09 +0000

    Added placeholder for ExecutorStub.

---
+++ Lucid.Domain.Testing/ExecutorStub.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Lucid.Domain.Execution;
+
+namespace Lucid.Domain.Testing
+{
+    public class ExecutorStub : IExecutor
+    {
+        private IList<object> _executed = new List<object>();
+
+        public IEnumerable<object> Executed { get { return _executed; } }
+
+        public object Execute(object executable)
+        {
+            _executed.Add(executable);
+            return null;
+        }
+    }
+}
---
+++ Lucid.Domain.Tests/Testing/ExecutorStubTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Lucid.Domain.Execution;
+using Lucid.Domain.Testing;
+using NUnit.Framework;
+
+namespace Lucid.Domain.Tests.Testing
+{
+    [TestFixture]
+    public class ExecutorStubTests
+    {
+        public class FakeVoidCommand : ICommand { }
+
+        [Test]
+        public void StoresAllExecutions()
+        {
+            var executor = new ExecutorStub();
+            var executable = new FakeVoidCommand();
+
+            executor.Execute(executable);
+
+            executor.Executed.Count().Should().Be(1);
+            executor.Executed.ElementAt(0).Should().Be(executable);
+        }
+    }
+}
