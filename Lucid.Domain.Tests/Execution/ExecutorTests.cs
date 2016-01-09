911c43d77e5d59b7c331a94076bec557faa5bc72
Richard Brown <flukefan@googlemail.com>
2016-01-09 14:43:40 +0000

    Added ExecutorTests.

---
+++ Lucid.Domain.Tests/Execution/ExecutorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Lucid.Domain.Execution;
+using NUnit.Framework;
+
+namespace Lucid.Domain.Tests.Execution
+{
+    [TestFixture]
+    public class ExecutorTests
+    {
+        [Test]
+        public void Executes()
+        {
+            var executor = new Executor();
+
+            var result = (int)executor.Execute(new SimpleExecutable());
+
+            result.Should().Be(3);
+        }
+
+        public class SimpleExecutable : QuerySingle<int>
+        {
+            public override int Find()
+            {
+                return 3;
+            }
+        }
+    }
+}
