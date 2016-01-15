68d5cd22c1a3b828b962660462c1535627ce4c12
Richard Brown <flukefan@googlemail.com>
2016-01-15 10:49:58 +0000

    Fixed intellisense, and package restore.

--- Demo/CommandPrompt.bat
+++ Demo/CommandPrompt.bat
@@ -1,6 +1,6 @@
 @CD /D "%~dp0"
 @title Demo Command Prompt
 @SET PATH=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\;%PATH%
-@doskey b=msbuild $* Demo.proj /p:RestorePackages=True
+@doskey b=msbuild Demo.proj /t:PackageRestore ^& msbuild $* Demo.proj
 type readme.txt
 %comspec%
