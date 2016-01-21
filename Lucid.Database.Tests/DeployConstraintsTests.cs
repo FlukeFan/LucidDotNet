48a0756fb2e7ce2ec191ef30927ede381b1e1abd
Richard Brown <flukefan@googlemail.com>
2016-01-21 18:43:19 +0000

    Downloaded a copy of the version deployed to NuGet so we can allow Demo to reference an older one while we work on a newer one.

---
+++ Lucid.Database.Tests/DeployConstraintsTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lucid.Database.Tests
+{
+    [TestFixture]
+    public class DeployConstraintsTests
+    {
+        [Test]
+        public void Verify_VersionIsUpdated_AfterDeployToNuGet()
+        {
+            var nugetExe = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\packages\\NuGet.CommandLine.3.3.0\\tools\\NuGet.exe");
+            var localCopyFolder = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\packages_deployed");
+            var localCopyVersionFile = Path.Combine(localCopyFolder, "version.txt");
+
+            var deployedVersion = GetDeployedVersion(nugetExe);
+            var localCopyVersion = GetLocalCopyVersion(localCopyVersionFile);
+
+            if (deployedVersion == null && localCopyVersionFile == null)
+                Assert.Fail("Cannot determine deployed version of Lucid");
+
+            if (deployedVersion != null && localCopyVersion != deployedVersion)
+                DownloadLocalCopy(nugetExe, localCopyFolder, localCopyVersionFile, deployedVersion);
+
+            localCopyVersion = GetLocalCopyVersion(localCopyVersionFile);
+
+            Assert.Ignore("Got current version: '" + localCopyVersion + "'");
+        }
+
+        private string GetDeployedVersion(string nugetExe)
+        {
+            var result = Run.Program(nugetExe, "list Lucid.Domain.Testing");
+
+            if (result.ExitCode != 0)
+            {
+                Console.WriteLine("Output from NuGet.exe:\n" + string.Join("\n", result.Output));
+                Console.WriteLine("Errors from NuGet.exe:\n" + string.Join("\n", result.Errors));
+                Console.WriteLine("Unable to get version from NuGet online");
+                return null;
+            }
+
+            var failMessage = "Output from NuGet.exe:\n" + string.Join("\n", result.Output);
+
+            if (result.Output.Count != 1)
+                Assert.Fail("Expected output to contain a single line, but got " + failMessage);
+
+            var lines = result.Output[0].Split(' ');
+
+            if (lines.Length != 2 || lines[0] != "Lucid.Domain.Testing")
+                Assert.Fail("Expected output to contain a single line, but got " + failMessage);
+
+            return lines[1];
+        }
+
+        private string GetLocalCopyVersion(string localCopyVersionFile)
+        {
+            if (!File.Exists(localCopyVersionFile))
+                return null;
+
+            return File.ReadAllText(localCopyVersionFile);
+        }
+
+        private void DownloadLocalCopy(string nugetExe, string localCopyFolder, string localCopyVersionFile, string deployedVersion)
+        {
+            // the internet has a more up-to-date copy, so download that
+
+            if (Directory.Exists(localCopyFolder))
+                Directory.Delete(localCopyFolder, true);
+
+            foreach (var nuspecFile in Directory.GetFiles("..\\..\\..\\", "Lucid.*.nuspec", SearchOption.AllDirectories))
+            {
+                var packageName = Path.GetFileNameWithoutExtension(nuspecFile);
+
+                Run.Program(nugetExe, string.Format("install {0} -o \"{1}\"", packageName, localCopyFolder));
+            }
+
+            File.WriteAllText(localCopyVersionFile, deployedVersion);
+        }
+    }
+}
---
+++ Lucid.Database.Tests/Run.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Lucid.Database.Tests
+{
+    public static class Run
+    {
+        public static RunResult Program(string fileName, string arguments)
+        {
+            return Program(fileName, arguments, Environment.CurrentDirectory);
+        }
+
+        public static RunResult Program(string fileName, string arguments, string workingDirectory)
+        {
+            var result = new RunResult();
+
+            using (Process process = new Process())
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.FileName = fileName;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.WorkingDirectory = workingDirectory;
+
+                using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
+                {
+                    process.OutputDataReceived += (sender, eventArgs) =>
+                    {
+                        if (eventArgs.Data == null)
+                            outputWaitHandle.Set();
+                        else
+                            result.Output.Add(eventArgs.Data);
+                    };
+
+                    process.ErrorDataReceived += (sender, eventArgs) =>
+                    {
+                        if (eventArgs.Data == null)
+                            outputWaitHandle.Set();
+                        else
+                            result.Errors.Add(eventArgs.Data);
+                    };
+
+                    process.Start();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+
+                    process.WaitForExit();
+
+                    result.ExitCode = process.ExitCode;
+                }
+            }
+
+            return result;
+        }
+
+        public class RunResult
+        {
+            public int              ExitCode;
+            public IList<string>    Output      = new List<string>();
+            public IList<string>    Errors      = new List<string>();
+        }
+    }
+}
