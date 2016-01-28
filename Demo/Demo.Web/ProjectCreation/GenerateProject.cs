4d5d6e3d1b54cf789e62830bbfb2ffb548c6f918
Richard Brown <flukefan@googlemail.com>
2016-01-28 15:53:40 +0000

    Started consolidation of packages (some packages.config files appear to have been removed)

---
+++ Demo/Demo.Web/ProjectCreation/GenerateProject.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Lucid.Domain.Execution;
+
+namespace Demo.Web.ProjectCreation
+{
+    public class GenerateProject : Command<byte[]>
+    {
+        public string Name { get; set; }
+
+        public override byte[] Execute()
+        {
+            var assembly = GetType().Assembly;
+            using (var zip = assembly.GetManifestResourceStream("Demo.Web.Project.zip"))
+                return StreamToBytes(zip);
+        }
+
+        private static byte[] StreamToBytes(Stream stream)
+        {
+            return null;
+        }
+    }
+}
