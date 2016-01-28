a72e0bd4739f431eae2d7ee45dd30af2e8822255
Richard Brown <flukefan@googlemail.com>
2016-01-28 19:45:09 +0000

    Added Demo.System.Tests placeholder project.

---
+++ Demo/Demo.System.Tests/ProjectCreation/GenerateProjectTests.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Web.ProjectCreation;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Demo.System.Tests.ProjectCreation
+{
+    [TestFixture]
+    [Explicit("Until this is working")]
+    public class GenerateProjectTests
+    {
+        [Test]
+        public void Execute()
+        {
+            var cmd = new GenerateProject { Name = "ShinyNewProject" };
+
+            var zipBytes = cmd.Execute();
+
+            zipBytes.Should().NotBeNull();
+            zipBytes.Length.Should().BeGreaterThan(0);
+        }
+    }
+}
