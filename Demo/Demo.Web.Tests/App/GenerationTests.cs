a3caf6ea55fa7e8f1fa3485f88faf942ab756f10
Richard Brown <flukefan@googlemail.com>
2016-02-02 13:58:47 +0000

    Added form for project generation.

---
+++ Demo/Demo.Web.Tests/App/GenerationTests.cs
@@ -0,0 +1,21 @@
+﻿using Demo.Web.App.Generation;
+using Demo.Web.Tests.Utility;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Demo.Web.Tests.App
+{
+    public class GenerationTests : WebTest
+    {
+        [Test]
+        public void Generate_GET_RendersForm()
+        {
+            WebAppTest(client =>
+            {
+                var response = client.Get(Actions.Generate());
+
+                response.Text.Should().Contain("<form");
+            });
+        }
+    }
+}
---
+++ Demo/Demo.Web/App/Generation/GenerationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Demo.Web.Utility;
+
+namespace Demo.Web.App.Generation
+{
+    public static class Actions
+    {
+        public static string Generate() { return "~/Generation/Generate"; }
+    }
+
+    public class GenerationController : DemoController
+    {
+        public ActionResult Generate()
+        {
+            return View();
+        }
+    }
+}
