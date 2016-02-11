160357f4018f2fc16190fb4248c59fd3ddc4e0f3
Richard Brown <flukefan@googlemail.com>
2016-02-11 14:40:47 +0000

    Increased default coverage to 85%

---
+++ Lucid.Web.Tests/MvcExecutorTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Lucid.Facade.Execution;
+using Lucid.Facade.Testing;
+using NUnit.Framework;
+
+namespace Lucid.Web.Tests
+{
+    [TestFixture]
+    public class MvcExecutorTests
+    {
+        [Test]
+        public void Exec_QueryList()
+        {
+            var stub = new ExecutorStub();
+            var executor = new CqExecutor(stub);
+            var expectedList = new List<int> { 1, 2, 3 };
+            stub.SetupQueryList(It.IsAny<QueryList>(), expectedList);
+
+            var result = MvcExecutor.Exec(executor, new QueryList());
+
+            result.Should().BeEquivalentTo(expectedList);
+        }
+
+        [Test]
+        public void Exec_QuerySingle()
+        {
+            var stub = new ExecutorStub();
+            var executor = new CqExecutor(stub);
+            stub.SetupQuerySingle(It.IsAny<QuerySingle>(), 5);
+
+            var result = MvcExecutor.Exec(executor, new QuerySingle());
+
+            result.Should().Be(5);
+        }
+
+        public class QueryList : IQueryList<int> { }
+        public class QuerySingle : IQuerySingle<int> { }
+    }
+}
