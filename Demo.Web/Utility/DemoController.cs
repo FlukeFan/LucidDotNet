f39377d1cdd9a1774b0a3386dc84378ea45c86e4
Richard Brown <flukefan@googlemail.com>
2016-09-17 16:14:45 +0100

    Pushed MvcExecutor back into Demo.Web to separate it from MVC Feature Folders

--- Demo.Web/Utility/DemoController.cs
+++ Demo.Web/Utility/DemoController.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Web.Mvc;
-using Lucid.Web;
 using SimpleFacade;
+using SimpleFacade.Exceptions;
+using SimpleFacade.Execution;
 
 namespace Demo.Web.Utility
 {
@@ -9,17 +10,62 @@ namespace Demo.Web.Utility
     {
         protected TReturn Exec<TReturn>(IQuery<TReturn> query)
         {
-            return MvcExecutor.Exec(PresentationRegistry.Executor, query);
+            return Executor().Execute(query);
         }
 
         protected ActionResult Exec(ICommand cmd, Func<ActionResult> success, Func<ActionResult> failure)
         {
-            return MvcExecutor.Exec(ModelState, PresentationRegistry.Executor, cmd, success, failure);
+            return Exec<object>(ModelState, () => { Executor().Execute(cmd); return null; }, nullValue => success(), failure);
         }
 
         protected ActionResult Exec<TReturn>(ICommand<TReturn> cmd, Func<TReturn, ActionResult> success, Func<ActionResult> failure)
         {
-            return MvcExecutor.Exec(ModelState, PresentationRegistry.Executor, cmd, success, failure);
+            return Exec(ModelState, () => Executor().Execute(cmd), success, failure);
+        }
+
+        private static ICqExecutor Executor()
+        {
+            return PresentationRegistry.Executor;
+        }
+
+        private static ActionResult Exec<TReturn>(ModelStateDictionary modelState, Func<TReturn> run, Func<TReturn, ActionResult> success, Func<ActionResult> failure)
+        {
+            TReturn response = default(TReturn);
+
+            if (IsValid(modelState))
+            {
+                try
+                {
+                    response = run();
+                }
+                catch (FacadeException exception)
+                {
+                    AddErrors(modelState, exception);
+                }
+            }
+
+            return IsValid(modelState)
+                ? success(response)
+                : failure();
+        }
+
+        private static bool IsValid(ModelStateDictionary modelState)
+        {
+            lock (modelState)
+                return modelState.IsValid;
+        }
+
+        private static void AddErrors(ModelStateDictionary modelState, FacadeException exception)
+        {
+            lock (modelState)
+            {
+                foreach (var message in exception.Messages)
+                    modelState.AddModelError("", message);
+
+                foreach (var kvp in exception.PropertyMessages)
+                    foreach (var message in kvp.Value)
+                        modelState.AddModelError(kvp.Key, message);
+            }
         }
     }
 }
