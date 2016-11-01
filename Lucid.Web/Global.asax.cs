2c088d73edd547d515c19576bed3b372cdf3350d
Richard Brown <flukefan@googlemail.com>
2016-11-01 15:15:03 +0000

    Pushed upgrade logic into its own class.

--- Lucid.Web/Global.asax.cs
+++ Lucid.Web/Global.asax.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Web;
-using System.Web.Hosting;
-using ICSharpCode.SharpZipLib.Core;
-using ICSharpCode.SharpZipLib.Zip;
 using Lucid.Web.Utility;
 using MvcFolders;
 
@@ -11,13 +6,11 @@ namespace Lucid.Web
 {
     public class LucidApplication : System.Web.HttpApplication
     {
-        private static bool _updating;
-
         public static Startup Startup = new Startup();
 
         protected void Application_Start()
         {
-            if (NewVersion())
+            if (Upgrader.InstalledNewVersion())
                 return;
 
             Startup.Init();
@@ -25,50 +18,10 @@ namespace Lucid.Web
             FeatureFolders.Register(typeof(LucidController).Assembly, "Lucid.Web.App");
         }
 
-        private bool NewVersion()
-        {
-            var packagedZip = HostingEnvironment.MapPath("~/Packaged.zip");
-
-            if (!File.Exists(packagedZip))
-                return false;
-
-            _updating = true;
-
-            var webFolder = HostingEnvironment.MapPath("~");
-            var buffer = new byte[4096];
-
-            using (var zipFile = new ZipFile(packagedZip))
-                foreach (ZipEntry zipEntry in zipFile)
-                {
-                    var outFile = Path.Combine(webFolder, zipEntry.Name);
-                    Directory.CreateDirectory(Path.GetDirectoryName(outFile));
-
-                    using (var streamWriter = File.Create(outFile))
-                        StreamUtils.Copy(zipFile.GetInputStream(zipEntry), streamWriter, buffer);
-                }
-
-            var deployedZip = HostingEnvironment.MapPath("~/Deployed.zip");
-
-            if (File.Exists(deployedZip))
-                File.Delete(deployedZip);
-
-            File.Move(packagedZip, deployedZip);
-
-            // in case the app hasn't recycled
-            HttpRuntime.UnloadAppDomain();
-
-            return true;
-        }
-
         protected void Application_BeginRequest(Object source, EventArgs e)
         {
-            if (_updating)
-            {
-                var app = (HttpApplication)source;
-                var context = app.Context;
-
-                context.Response.Redirect(context.Request.Url.ToString(), false);
-            }
+            if (Upgrader.BeginRequest(source))
+                return;
         }
     }
 }
---
+++ Lucid.Web/Utility/Upgrader.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Hosting;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace Lucid.Web.Utility
+{
+    public class Upgrader
+    {
+        private static bool _updating;
+
+        public static bool InstalledNewVersion()
+        {
+            var packagedZip = HostingEnvironment.MapPath("~/Packaged.zip");
+
+            if (!File.Exists(packagedZip))
+                return false;
+
+            _updating = true;
+
+            var webFolder = HostingEnvironment.MapPath("~");
+            var buffer = new byte[4096];
+
+            using (var zipFile = new ZipFile(packagedZip))
+                foreach (ZipEntry zipEntry in zipFile)
+                {
+                    var outFile = Path.Combine(webFolder, zipEntry.Name);
+                    Directory.CreateDirectory(Path.GetDirectoryName(outFile));
+
+                    using (var streamWriter = File.Create(outFile))
+                        StreamUtils.Copy(zipFile.GetInputStream(zipEntry), streamWriter, buffer);
+                }
+
+            var deployedZip = HostingEnvironment.MapPath("~/Deployed.zip");
+
+            if (File.Exists(deployedZip))
+                File.Delete(deployedZip);
+
+            File.Move(packagedZip, deployedZip);
+
+            // in case the app hasn't recycled
+            HttpRuntime.UnloadAppDomain();
+
+            return true;
+        }
+
+        public static bool BeginRequest(object source)
+        {
+            if (!_updating)
+                return false;
+
+            var app = (HttpApplication)source;
+            var context = app.Context;
+
+            // redirect back to the same URL until the update is complete
+            context.Response.Redirect(context.Request.Url.ToString(), false);
+
+            return true;
+        }
+    }
+}
