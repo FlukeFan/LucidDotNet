7e6eaa35e06638b86f3ba7c10abdac0f0408c896
Richard Brown <flukefan@googlemail.com>
2016-11-18 10:54:31 +0000

    Deployed Y2016.M11.V01

--- Lucid.Database/Migrations/DeployedMigrations.cs
+++ Lucid.Database/Migrations/DeployedMigrations.cs
@@ -10,6 +10,7 @@ namespace Lucid.Database.Migrations
         static DeployedMigrations()
         {
             Hashes.Add(typeof(Y2016.M01.V01), "B0C8A4259C22EB970217DEE2E9A82BA5");
+            Hashes.Add(typeof(Y2016.M11.V01), "6FC894EA5FFEF8B3B10AEB3CD99493F4");
         }
     }
 }
