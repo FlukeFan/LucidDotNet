87a47421a3bdf8e99bedf275f5d2ad2c23e612a6
Richard Brown <flukefan@googlemail.com>
2017-09-11 16:17:15 +0100

    Added logic to delete 'extra' files/folders that are not in the package (excluding any root folder/files)

--- Lucid.Web/Utility/Upgrader.cs
+++ Lucid.Web/Utility/Upgrader.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Hosting;
 using ICSharpCode.SharpZipLib.Core;
@@ -22,16 +26,57 @@ namespace Lucid.Web.Utility
             var webFolder = HostingEnvironment.MapPath("~");
             var buffer = new byte[4096];
 
+            var unzippedFolders = new List<string>();
+            var unzippedFiles = new List<string>();
+
             using (var zipFile = new ZipFile(packagedZip))
                 foreach (ZipEntry zipEntry in zipFile)
                 {
                     var outFile = Path.Combine(webFolder, zipEntry.Name);
-                    Directory.CreateDirectory(Path.GetDirectoryName(outFile));
+                    var folder = Path.GetDirectoryName(outFile);
+
+                    if (!unzippedFolders.Contains(Normalise(folder)))
+                        unzippedFolders.Add(Normalise(folder));
+
+                    Directory.CreateDirectory(folder);
 
                     if (zipEntry.IsFile)
+                    {
+                        if (!unzippedFiles.Contains(Normalise(outFile)))
+                            unzippedFiles.Add(Normalise(outFile));
+
                         using (var streamWriter = File.Create(outFile))
                             StreamUtils.Copy(zipFile.GetInputStream(zipEntry), streamWriter, buffer);
+                    }
                 }
+
+            // delete any 'extra' files/folders if we're not in debug
+            if (!HttpContext.Current.IsDebuggingEnabled)
+            {
+                // ignore extra files/folder in the root web folder (e.g., log files)
+                if (unzippedFolders.Contains(Normalise(webFolder)))
+                    unzippedFolders.Remove(Normalise(webFolder));
+
+                var extraFolders = new List<string>();
+                var extraFiles = new List<string>();
+
+                foreach (var unzippedFolder in unzippedFolders.OrderByDescending(f => f.Length))
+                {
+                    foreach (var subFolder in Directory.GetDirectories(unzippedFolder))
+                        if (!unzippedFolders.Contains(Normalise(subFolder)))
+                            extraFolders.Add(subFolder);
+
+                    foreach (var file in Directory.GetFiles(unzippedFolder))
+                        if (!unzippedFiles.Contains(Normalise(file)))
+                            extraFiles.Add(file);
+                }
+
+                foreach (var file in extraFiles)
+                    Retry(() => File.Delete(file));
+
+                foreach (var folder in extraFolders)
+                    Retry(() => Directory.Delete(folder, true));
+            }
 
             var deployedZip = HostingEnvironment.MapPath("~/Deployed.zip");
 
@@ -58,6 +103,28 @@ namespace Lucid.Web.Utility
             context.Response.Redirect(context.Request.Url.ToString(), false);
 
             return true;
+        }
+
+        private static string Normalise(string path)
+        {
+            path = path.Replace("\\", "/");
+            path = path.TrimEnd('/');
+            path = path.ToLower(); // should only do this on case-insensitive file systems
+            return path;
+        }
+
+        private static void Retry(Action action)
+        {
+            for (var i = 0; i < 3; i++)
+                try
+                {
+                    action();
+                    return;
+                }
+                catch
+                {
+                    Thread.Sleep(10);
+                }
         }
     }
 }
