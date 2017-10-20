fc2346959b6423c2db4a25147d35447c5d561929
Richard Brown <flukefan@googlemail.com>
2017-10-20 12:00:56 +0100

    Replaced form-actions css class with helper that generates bootstrap row.

---
+++ Lucid.Web/App/Helpers/LucidHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using MvcForms;
+
+namespace Lucid.Web.App.Helpers
+{
+    public static class LucidHelpers
+    {
+        public static ScopedHtmlHelper<T> BeginFormButtons<T>(this HtmlHelper<T> helper)
+        {
+            helper.ViewContext.Writer.Write("<div class=\"row\"><div class=\"col-xs-offset-4 col-xs-8\">");
+            return new ScopedHtmlHelper<T>(helper, () =>
+            {
+                helper.ViewContext.Writer.Write("</div></div>");
+            });
+        }
+    }
+}
