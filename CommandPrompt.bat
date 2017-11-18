2272f8cb18f46a4b7b9bc8c6cdd3907db507a778
Richard Brown <flukefan@googlemail.com>
2017-11-18 11:47:55 +0000

    Switched to msbuild.exe 15 from command prompt

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -1,6 +1,6 @@
 @CD /D "%~dp0"
 @title Lucid Command Prompt
-@SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\;%PATH%
+@SET PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\;%PATH%
 @doskey b=msbuild $* Lucid.proj
 type readme.txt
 %comspec%
