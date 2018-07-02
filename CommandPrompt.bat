480658e048090dc673557524579d8ce9c3159ef4
Richard Brown <fluke_fan@hotmail.com>
2018-07-02 07:31:02 +0100

    Added restore target.

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -2,7 +2,7 @@
 @title Lucid Command Prompt
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 type readme.txt
-@doskey bc=dotnet msbuild build.proj /t:clean
+@doskey bc=dotnet clean build.proj
 @doskey btw=dotnet watch msbuild build.proj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
 @doskey bt=dotnet msbuild build.proj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
 @doskey bw=dotnet watch msbuild build.proj $*
