44484fd03645ce67d1a55a546f3a3a5e254cc9f7
Richard Brown <fluke_fan@hotmail.com>
2018-07-02 07:19:45 +0100

    Started new build structure (now that watch is part of SDK).

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -3,14 +3,13 @@
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 type readme.txt
 @doskey bc=dotnet clean
-@doskey btw=dotnet watch msbuild Build.csproj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
-@doskey bt=dotnet msbuild Build.csproj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
-@doskey bw=dotnet watch msbuild Build.csproj $*
-@doskey b=dotnet msbuild Build.csproj $*
-@doskey br=dotnet restore Build.csproj $*
+@doskey btw=dotnet watch msbuild build.proj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
+@doskey bt=dotnet msbuild build.proj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
+@doskey bw=dotnet watch msbuild build.proj $*
+@doskey b=dotnet msbuild build.proj $*
+@doskey br=dotnet restore build.proj $*
 @echo.
 @echo Aliases:
 @echo.
 @doskey /MACROS
-@CD Build
 %comspec%
