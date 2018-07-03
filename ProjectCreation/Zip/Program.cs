bdeb4323824f619a352a80375fc9153d90a40d13
Richard Brown <fluke_fan@hotmail.com>
2018-07-03 07:19:01 +0100

    Added initial zipping of project.

--- ProjectCreation/Zip/Program.cs
+++ ProjectCreation/Zip/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 
-namespace Zip
+namespace Lucid.ProjectCreation.Zip
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootFolder = Path.GetFullPath(args[0]);
+            var zipFile = Path.GetFullPath(args[1]);
+            var filesToZip = args[2].Split(';');
+
+            using (var stream = File.Create(zipFile))
+            using (var archive = new ZipArchive(stream, ZipArchiveMode.Create, false))
+            {
+                foreach (var fileToZip in filesToZip)
+                {
+                    var entryName = Path.GetFileName(fileToZip);
+                    var entry = archive.CreateEntryFromFile(fileToZip, entryName);
+                    Console.WriteLine($"Zipped {entryName}");
+                }
+            }
         }
     }
 }
