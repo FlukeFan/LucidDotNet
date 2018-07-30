1248b3f159f75e81505c906504c34487ffccc598
Richard Brown <fluke_fan@hotmail.com>
2018-07-30 19:03:01 +0100

    Changed default of RunTests=true (passed by default when building from the command line).

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -4,10 +4,10 @@
 @SET PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\;%PATH%
 type readme.txt
 @doskey bc=dotnet clean build.proj
-@doskey btw=dotnet watch msbuild build.proj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
-@doskey bt=dotnet msbuild build.proj /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
-@doskey bw=dotnet watch msbuild build.proj $*
-@doskey b=dotnet msbuild build.proj $*
+@doskey btw=dotnet watch msbuild build.proj /p:RunTests=true /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
+@doskey bt=dotnet msbuild build.proj /p:RunTests=true /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
+@doskey bw=dotnet watch msbuild build.proj /p:RunTests=true $*
+@doskey b=dotnet msbuild build.proj /p:RunTests=true $*
 @doskey br=dotnet restore build.proj $*
 @echo.
 @echo Aliases:
