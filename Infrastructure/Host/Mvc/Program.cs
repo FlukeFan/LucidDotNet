2ae88dfeef6285b645ae7ae9e94d012eba98e05b
Richard Brown <fluke_fan@hotmail.com>
2018-08-21 10:30:14 +0100

    Added NLog logging.

--- Infrastructure/Host/Mvc/Program.cs
+++ Infrastructure/Host/Mvc/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using NLog;
+using NLog.Web;
 
 namespace WebApp
 {
@@ -7,11 +11,30 @@ namespace WebApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            if (File.Exists("nlog.config"))
+                NLogBuilder.ConfigureNLog("nlog.config");
+
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Info($"Creating web host");
+
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "caught error");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseNLog()
                 .UseStartup<Startup>();
     }
 }
