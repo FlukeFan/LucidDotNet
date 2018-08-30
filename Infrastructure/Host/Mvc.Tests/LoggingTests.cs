5ce94557cf190526e444e41d92d2fb5b895c1b5f
Richard Brown <fluke_fan@hotmail.com>
2018-08-30 14:48:22 +0100

    Preserved modified variables on server when deploying.

--- Infrastructure/Host/Mvc.Tests/LoggingTests.cs
+++ Infrastructure/Host/Mvc.Tests/LoggingTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using FluentAssertions;
 using NUnit.Framework;
@@ -60,7 +61,7 @@ namespace Lucid.Infrastructure.Host.Mvc.Tests
 
             var file = Logging.PrepareConfigFile();
 
-            File.ReadAllText(file).Should().Be("<nlog>newer</nlog>");
+            File.ReadAllText(file).Should().EndWith("<nlog>newer</nlog>");
         }
 
         [Test]
@@ -71,6 +72,37 @@ namespace Lucid.Infrastructure.Host.Mvc.Tests
 
             File.WriteAllText("nlog.config", "<nlog>newer</nlog>");
             Logging.PrepareConfigFile();
+        }
+
+        [Test]
+        public void WhenConfigUpdated_VariablesAreRetained()
+        {
+            Directory.CreateDirectory("../logs.config");
+
+            File.WriteAllText("../logs.config/nlog.mvc.config", "<nlog xmlns=\"http://www.nlog-project.org/schemas/NLog.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
+                + "<variable name=\"name2\" value=\"server_secret\" />"
+                + "</nlog>");
+
+            File.WriteAllText("nlog.config", "<nlog xmlns=\"http://www.nlog-project.org/schemas/NLog.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
+                + "<variable name=\"name1\" value=\"value1\" />"
+                + "<variable name=\"name2\" value=\"value2\" />"
+                + "<variable name=\"name3\" value=\"value3\" />"
+                + "</nlog>");
+
+
+            var old = DateTime.UtcNow - TimeSpan.FromHours(3);
+            File.SetLastWriteTimeUtc("../logs.config/nlog.mvc.config", old);
+
+            var file = Logging.PrepareConfigFile();
+
+            var values = Logging.ReadVariables("../logs.config/nlog.mvc.config");
+
+            values.Should().BeEquivalentTo(new Dictionary<string, string>()
+            {
+                { "name1", "value1" },
+                { "name2", "server_secret" },
+                { "name3", "value3" },
+            });
         }
     }
 }
--- Infrastructure/Host/Mvc/Logging.cs
+++ Infrastructure/Host/Mvc/Logging.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
 
 namespace Lucid.Infrastructure.Host.Mvc
 {
@@ -27,6 +29,7 @@ namespace Lucid.Infrastructure.Host.Mvc
             if (targetModified > sourceModified)
                 return targetLogFile;
 
+            var existingVariables = ReadVariables(targetLogFile);
             var previous = $"{targetLogFile}.previous";
 
             if (File.Exists(previous))
@@ -34,9 +37,43 @@ namespace Lucid.Infrastructure.Host.Mvc
 
             File.Move(targetLogFile, $"{targetLogFile}.previous");
 
-            File.Copy(sourceLogFile, targetLogFile);
+            var doc = XDocument.Load(sourceLogFile, LoadOptions.PreserveWhitespace);
+
+            var variableElements = GetVariableElements(doc);
+
+            foreach (var variableElement in variableElements)
+            {
+                var name = variableElement.Attribute("name").Value;
+
+                if (!existingVariables.ContainsKey(name))
+                    continue;
+
+                variableElement.Attribute("value").Value = existingVariables[name];
+            }
+
+            doc.Save(targetLogFile);
 
             return targetLogFile;
+        }
+
+        public static IDictionary<string, string> ReadVariables(string xmlFile)
+        {
+            var variables = new Dictionary<string, string>();
+
+            var doc = XDocument.Load(xmlFile);
+            var variableElements = GetVariableElements(doc);
+
+            foreach (var variableElement in variableElements)
+                variables.Add(variableElement.Attribute("name").Value, variableElement.Attribute("value").Value);
+
+            return variables;
+        }
+
+        private static IEnumerable<XElement> GetVariableElements(XDocument doc)
+        {
+            var ns = doc.Root.GetDefaultNamespace();
+            var variableElements = doc.Descendants(ns + "variable");
+            return variableElements;
         }
     }
 }
