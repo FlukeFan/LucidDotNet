d765314321f5a75a4e4550504d88ffdba63b2be5
Richard Brown <fluke_fan@hotmail.com>
2018-09-03 16:51:51 +0100

    Added increasing timeouts to error emails (so we don't flood/DoS email server).

--- Infrastructure/Host/Mvc/Logging.cs
+++ Infrastructure/Host/Mvc/Logging.cs
@@ -1,11 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
+using NLog.Common;
+using NLog.Targets;
+using NLog.Targets.Wrappers;
 
 namespace Lucid.Infrastructure.Host.Mvc
 {
     public class Logging
     {
+        /// <summary> Custom NLog buffer to batch emails in increasing batch sizes </summary>
+        [Target("VariableBuffer", IsWrapper = true)]
+        public class VariableBuffer : BufferingTargetWrapper
+        {
+            private DateTime    _lastWrite;
+            private int         _waitFactor;
+
+            public VariableBuffer()
+            {
+                _lastWrite = DateTime.MinValue;
+                _waitFactor = 1;
+                BufferSize = 100000;
+                SlidingTimeout = false;
+            }
+
+            protected override void Write(AsyncLogEventInfo logEvent)
+            {
+                const int maxMins = 100 * 60 * 1000;
+                const int reduceTimeoutMins = 30;
+
+                var now = DateTime.UtcNow;
+
+                if (FactorTimeout() < maxMins)
+                    _waitFactor = _waitFactor * 2;
+
+                var decreaseThreshold = _lastWrite + TimeSpan.FromMinutes(reduceTimeoutMins);
+
+                while (_waitFactor > 1 && now > decreaseThreshold)
+                {
+                    _waitFactor = _waitFactor / 2;
+                    decreaseThreshold += TimeSpan.FromMinutes(reduceTimeoutMins);
+                }
+
+                FlushTimeout = FactorTimeout();
+                _lastWrite = now;
+
+                InternalLogger.Trace($"VariableBuffer: FlushTimeout={FlushTimeout}");
+
+                base.Write(logEvent);
+            }
+
+            private int FactorTimeout()
+            {
+                var timeout = _waitFactor * 1000;
+                return timeout;
+            }
+        }
+
         public static string PrepareConfigFile()
         {
             var sourceLogFile = Path.GetFullPath("nlog.config");
