b8c1f30501496c543b19af0fd34aa0f5d315e032
Richard Brown <fluke_fan@hotmail.com>
2018-09-05 09:11:27 +0100

    Added first stab at controller test.

---
+++ ProjectCreation/Tests/ProjectCreationControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using MvcTesting.AspNetCore;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ProjectCreationControllerTests
+    {
+        private TestServer _testServer;
+
+        [OneTimeSetUp]
+        public void SetUpFixture()
+        {
+            _testServer = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseContentRoot(@"..\..\..\..\Module")
+                .UseStartup<TestStartup>()
+                .MvcTestingTestServer();
+        }
+
+        [Test]
+        public async Task Index_DisplaysForm()
+        {
+            var response = await _testServer.MvcTestingClient()
+                .GetAsync("/ProjectCreation");
+
+            response.Text.Should().StartWith("Hello from deployed");
+        }
+    }
+
+    public class TestStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+            });
+        }
+    }
+}
