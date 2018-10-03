7f402a5ceac9fdb120c3bbfb5aa32b830558cbae
Richard Brown <fluke_fan@hotmail.com>
2018-10-03 15:04:24 +0100

    Added base class for razor pages.

---
+++ Infrastructure/Lib/MvcApp/MvcAppPage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Lucid.Infrastructure.Lib.MvcApp
+{
+    public abstract class MvcAppPage<TModel> : RazorPage<TModel>
+    {
+        public void SetTitle(string title)
+        {
+        }
+    }
+}
