23c5724b0caab765ea849ee1f7b3575a3887d2f5
Richard Brown <fluke_fan@hotmail.com>
2018-10-06 16:54:24 +0100

    Added simple cache-busting to CSS (will work for now, but need to figure out later how to handle this with P-JAX).

---
+++ Infrastructure/Host/Web/Layout/RazorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Internal;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lucid.Infrastructure.Host.Web.Layout
+{
+    public static class RazorExtensions
+    {
+        // from:  https://stackoverflow.com/a/45088852/357728
+        public static string AddFileVersionToPath(this HttpContext context, string path)
+        {
+            IMemoryCache cache = context.RequestServices.GetRequiredService<IMemoryCache>();
+            var hostingEnvironment = context.RequestServices.GetRequiredService<IHostingEnvironment>();
+            var versionProvider = new FileVersionProvider(hostingEnvironment.WebRootFileProvider, cache, context.Request.Path);
+            return versionProvider.AddFileVersionToPath(path);
+        }
+    }
+}
