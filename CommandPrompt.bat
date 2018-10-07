ee9d184b3de717fbc4d7a967fff433d2b849b688
Richard Brown <fluke_fan@hotmail.com>
2018-10-07 14:58:54 +0100

    Removed generated files from source control.

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -3,9 +3,9 @@
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 @SET PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\;%PATH%
 type readme.md
-@doskey webw=dotnet watch run -p Infrastructure\Host\Web\Lucid.Infrastructure.Host.Web.csproj
-@doskey web=dotnet run -p Infrastructure\Host\Web\Lucid.Infrastructure.Host.Web.csproj
-@doskey bc=dotnet clean build.proj
+@doskey webw=dotnet watch run -p Infrastructure\Host\Web\Lucid.Infrastructure.Host.Web.csproj $*
+@doskey web=dotnet run -p Infrastructure\Host\Web\Lucid.Infrastructure.Host.Web.csproj $*
+@doskey bc=dotnet clean build.proj $*
 @doskey btw=dotnet watch msbuild build.proj /p:RunTests=true /m:16 /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
 @doskey bt=dotnet msbuild build.proj /p:RunTests=true /m:16 /p:FilterTestFqn=$1 $2 $3 $4 $5 $6 $7 $8 $9
 @doskey bw=dotnet watch msbuild build.proj /p:RunTests=true /p:NoCoverage=true /m:16 $*
