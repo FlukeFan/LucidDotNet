3ae06bb505ab4d1da5583a0b7c55a611df1083ff
Richard Brown <fluke_fan@hotmail.com>
2018-12-17 10:58:57 +0000

    Updated to asp.net 2.2

--- Infrastructure/Host/Web/Layout/RazorExtensions.cs
+++ Infrastructure/Host/Web/Layout/RazorExtensions.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.TagHelpers.Internal;
-using Microsoft.Extensions.Caching.Memory;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Lucid.Infrastructure.Host.Web.Layout
@@ -11,10 +9,8 @@ namespace Lucid.Infrastructure.Host.Web.Layout
         // from:  https://stackoverflow.com/a/45088852/357728
         public static string AddFileVersionToPath(this HttpContext context, string path)
         {
-            IMemoryCache cache = context.RequestServices.GetRequiredService<IMemoryCache>();
-            var hostingEnvironment = context.RequestServices.GetRequiredService<IHostingEnvironment>();
-            var versionProvider = new FileVersionProvider(hostingEnvironment.WebRootFileProvider, cache, context.Request.Path);
-            return versionProvider.AddFileVersionToPath(path);
+            var versionProvider = context.RequestServices.GetRequiredService<IFileVersionProvider>();
+            return versionProvider.AddFileVersionToPath(context.Request.Path, path);
         }
     }
 }
