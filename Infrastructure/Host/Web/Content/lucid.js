52113dfbf607eabc186cbd4a16e27732ce9295f4
Richard Brown <fluke_fan@hotmail.com>
2019-01-03 17:04:17 +0000

    Wired up client-side pjax.

--- Infrastructure/Host/Web/Content/lucid.js
+++ Infrastructure/Host/Web/Content/lucid.js
@@ -1,5 +1,12 @@
 
 (function () {
+    var pjaxOnError = mfoPjax.onError;
 
+    mfoPjax.onError = function (jqXHR, textStatus, errorThrown, context, callback) {
+        if (!pjaxOnError(jqXHR, textStatus, errorThrown, context, callback))
+            alert("Error: " + textStatus);
+    };
+
+    mvcForms.init();
 }());
 
