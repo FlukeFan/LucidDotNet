2a3143455ea08671a4cb7789552f9dde1c837366
Richard Brown <fluke_fan@hotmail.com>
2019-01-04 12:02:05 +0000

    Added filter of slow tests for quick build.

---
+++ Infrastructure/Lib/Testing/SlowTest.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Lucid.Infrastructure.Lib.Testing
+{
+    public class SlowTest : CategoryAttribute
+    {
+        public SlowTest() : base("Slow") { }
+    }
+}
