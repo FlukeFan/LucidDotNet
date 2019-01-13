e0d5cb9124ace5aa7d6cfda2f9d716406c530a40
Richard Brown <fluke_fan@hotmail.com>
2019-01-13 14:08:43 +0000

    Added Account module.

---
+++ Modules/Account/Tests/Fixture.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Lucid.Modules.Account.Tests
+{
+    [TestFixture]
+    public class Fixture
+    {
+        [Test]
+        public void Test()
+        {
+            "Placeholder".Should().NotBeNull();
+        }
+    }
+}
