1a6dea4b4b059358f1fe667fc3d7d7b817fb051d
Richard Brown <fluke_fan@hotmail.com>
2019-01-20 11:07:07 +0000

    Simplified migration (append only now).

--- Infrastructure/Lib/Domain/SqlServer/SqlServerMigration.cs
+++ Infrastructure/Lib/Domain/SqlServer/SqlServerMigration.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentMigrator;
 using FluentMigrator.Builders.Create.Table;
 using FluentMigrator.Builders.Schema;
@@ -10,13 +8,15 @@ namespace Lucid.Infrastructure.Lib.Domain.SqlServer
 {
     public class MigrationOrderAttribute : MigrationAttribute
     {
-        public MigrationOrderAttribute(int major, int minor = 0, int script = 0) : base(((major * 1000 + minor) * 1000 + script) * 1000)
+        public MigrationOrderAttribute(int major, int minor = 0, int script = 0, int rev = 0) : base(((major * 1000 + minor) * 1000 + script) * 1000 + rev)
         {
         }
     }
 
     public abstract class SqlServerMigration : Migration
     {
+        public SqlServerMigration() : this(null) { }
+
         public SqlServerMigration(DbQuery dbQuery)
         {
             DbQuery = dbQuery;
@@ -29,45 +29,6 @@ namespace Lucid.Infrastructure.Lib.Domain.SqlServer
         public ISchemaExpressionRoot Db { get { return base.Schema; } }
 
         public new ISchemaSchemaSyntax Schema { get { return Db.Schema(DbQuery.SchemaName);  } }
-
-
-        protected void RemoveFromVersionInfoTable<T>(ExistingVersionsInfo versionsInfo)
-        {
-            var migrationAttribute = (MigrationAttribute)typeof(T).GetCustomAttributes(true).SingleOrDefault(a => typeof(MigrationAttribute).IsAssignableFrom(a.GetType()));
-            var version = migrationAttribute.Version;
-
-            if (!versionsInfo.Versions.Contains(version))
-                return;
-
-            Execute.Sql($@"
-                UPDATE {SchemaVersionMetadata.DefaultTableName}
-                SET {SchemaVersionMetadata.DefaultColumnName} = {versionsInfo.RerunIndex--}
-                 , {SchemaVersionMetadata.DefaultDescriptionColumnName} = 'Original({version}) ' + {SchemaVersionMetadata.DefaultDescriptionColumnName}
-                WHERE {SchemaVersionMetadata.DefaultColumnName} = {version}");
-        }
-
-        protected ExistingVersionsInfo GetVersionsInfo()
-        {
-            var versionsInfo = new ExistingVersionsInfo();
-
-            if (Schema.Table("VersionInfo").Exists())
-            {
-                var sql = $"SELECT {SchemaVersionMetadata.DefaultColumnName} FROM {SchemaVersionMetadata.DefaultTableName}";
-                versionsInfo.Versions = DbQuery.ExecuteList<long>(sql);
-            }
-
-            versionsInfo.RerunIndex = versionsInfo.Versions.Count > 0
-                ? Math.Min(versionsInfo.Versions.Min(), -1)
-                : -1;
-
-            return versionsInfo;
-        }
-
-        protected class ExistingVersionsInfo
-        {
-            public IList<long>  Versions    = new List<long>();
-            public long         RerunIndex;
-        }
     }
 
     public static class CreateTableExtensions
--- Modules/Account/Module/DbMigrations/V001/V000/Script000.cs
+++ Modules/Account/Module/DbMigrations/V001/V000/Script000.cs
@@ -3,15 +3,10 @@
 namespace Lucid.Modules.Account.DbMigrations.V001.V000
 {
     [MigrationOrder(major: 1, minor: 0, script: 0)]
-    public class Script000 : SqlServerMigration
+    public class Rev0_CreateUserTable : SqlServerMigration
     {
-        public Script000(DbQuery dbQuery) : base(dbQuery) { }
-
         public override void Up()
         {
-            if (Schema.Table("User").Exists())
-                Delete.Table("User");
-
             Create.Table("User")
                 .LucidPrimaryKey("User", "Id")
                 .WithColumn("Name").AsAnsiString().NotNullable()
