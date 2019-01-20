60be52e548196634e486835730d3ac49a3ae2f63
Richard Brown <fluke_fan@hotmail.com>
2019-01-20 11:11:09 +0000

    Inlined constants.

--- Infrastructure/Lib/Domain/SqlServer/SchemaVersionMetadata.cs
+++ Infrastructure/Lib/Domain/SqlServer/SchemaVersionMetadata.cs
@@ -4,22 +4,18 @@ namespace Lucid.Infrastructure.Lib.Domain.SqlServer
 {
     public class SchemaVersionMetadata : IVersionTableMetaData
     {
-        public const string DefaultTableName                = "VersionInfo";
-        public const string DefaultColumnName               = "Version";
-        public const string DefaultDescriptionColumnName    = "Description";
-
         public SchemaVersionMetadata(string schemaName)
         {
             SchemaName = schemaName;
         }
 
         public bool     OwnsSchema              => true;
-        public string   ColumnName              => DefaultColumnName;
+        public string   ColumnName              => "Version";
         public string   SchemaName              { get; }
-        public string   TableName               => DefaultTableName;
+        public string   TableName               => "VersionInfo";
         public string   UniqueIndexName         => "UC_Version";
         public string   AppliedOnColumnName     => "AppliedOn";
-        public string   DescriptionColumnName   => DefaultDescriptionColumnName;
+        public string   DescriptionColumnName   => "Description";
         public object   ApplicationContext      { get; set; }
     }
 }
