6bb457020f61a2555bb4619b00d4b4af4ab07300
Richard Brown <fluke_fan@hotmail.com>
2019-02-06 11:05:54 +0000

    Added initial test that user can login.

--- Modules/Account/Module/Controller.cs
+++ Modules/Account/Module/Controller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lucid.Modules.Account
 {
@@ -6,7 +7,8 @@ namespace Lucid.Modules.Account
     {
         internal const string RoutePrefix = "account";
 
-        public static string Index() { return $"/{RoutePrefix}"; }
+        public static string Index()        { return $"/{RoutePrefix}"; }
+        public static string LoginSuccess() { return $"/{RoutePrefix}/loginSuccess"; }
     }
 
     [Route(Actions.RoutePrefix)]
@@ -15,17 +17,32 @@ namespace Lucid.Modules.Account
         [HttpGet]
         public IActionResult Index()
         {
-            return Render(new Login());
+            return RenderIndex(new Login());
         }
 
-        private IActionResult Render(Login cmd)
+        [HttpPost]
+        public async Task<IActionResult> Index(Login cmd)
         {
-            var model = new IndexModel
-            {
-                Cmd = cmd,
-            };
+            return await ExecAsync(cmd,
+                success: user => Login(user),
+                failure: () => null);
+        }
 
+        private IActionResult RenderIndex(Login cmd)
+        {
+            var model = new IndexModel { Cmd = cmd };
             return View(model);
+        }
+
+        private IActionResult Login(User user)
+        {
+            return Redirect(Actions.LoginSuccess());
+        }
+
+        [HttpGet("loginSuccess")]
+        public IActionResult LoginSuccess()
+        {
+            return View();
         }
     }
 }
--- Modules/Account/Tests/ControllerTests.cs
+++ Modules/Account/Tests/ControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using FluentAssertions;
 using Lucid.Infrastructure.Lib.Testing.Controller;
+using Microsoft.AspNetCore.Mvc;
 using MvcTesting.Html;
 using NUnit.Framework;
 
@@ -19,6 +20,29 @@ namespace Lucid.Modules.Account.Tests
             form.Action.Should().Be(Actions.Index().PrefixLocalhost());
 
             form.GetText(m => m.UserName).Should().Be("");
+        }
+
+        [Test]
+        public async Task Can_Login()
+        {
+            var client = MvcTestingClient();
+
+            var form = await client
+                .GetAsync(Actions.Index())
+                .Form<Login>();
+
+            var response = await form
+                .SetText(m => m.UserName, "User1")
+                .Submit();
+
+            ExecutorStub.SingleExecuted<Login>().Should().BeEquivalentTo(new Login { UserName = "User1" });
+
+            // TODO: assert that login cookie is set
+
+            var redirectUrl = response.LastResult.As<RedirectResult>().Url;
+            redirectUrl.Should().Be(Actions.LoginSuccess());
+
+            await client.GetAsync(redirectUrl);
         }
     }
 }
