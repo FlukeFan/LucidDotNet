e427f3a2134ee3a00c0c1affa2e0a130fdb77bb4
Richard Brown <fluke_fan@hotmail.com>
2019-02-25 15:23:50 +0000

    Added empty module (not part of build yet).

---
+++ Modules/AppFactory/Design/Tests/ModuleTestSetup.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Lucid.Infrastructure.Lib.Domain.SqlServer;
+using Lucid.Infrastructure.Lib.Testing;
+using Lucid.Infrastructure.Lib.Testing.Controller;
+using Lucid.Infrastructure.Lib.Testing.Execution;
+using Lucid.Infrastructure.Lib.Testing.SqlServer;
+using MvcTesting.AspNetCore;
+using NHibernate;
+using NUnit.Framework;
+using Reposify.Testing;
+
+namespace Lucid.Modules.AppFactory.Design.Tests
+{
+    [SetUpFixture]
+    public class ModuleTestSetup
+    {
+        public static ConstraintChecker MemoryConstraints = new ConstraintChecker();
+
+        private static Lazy<ISessionFactory>            _sessionFactory;
+        private static SetupTestServer<TestStartup>     _testServerSetup;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            //var schema =
+            //    SqlTestUtil.UpdateMigrations<DbMigrations.V001.V000.Rev0_CreateUserTable>(
+            //        schemaName:             "Security",
+            //        migrationsSourceFolder: Path.Combine(TestUtil.ProjectPath(), "../Module/DbMigrations"));
+
+            //_testServerSetup = new SetupTestServer<TestStartup>();
+
+            //_sessionFactory = new Lazy<ISessionFactory>(() => Registry.BuildSessionFactory(schema.ConnectionString));
+
+            _testServerSetup = null;
+            _sessionFactory = null;
+        }
+
+        [OneTimeTearDown]
+        public void OnTimeTearDown()
+        {
+            using (_testServerSetup) { }
+
+            if (_sessionFactory.IsValueCreated)
+                using (_sessionFactory.Value) { }
+        }
+
+        [TestFixture]
+        public abstract class ControllerTest
+        {
+            private SetupExecutorStub _excecutorStub;
+
+            protected SimulatedHttpClient   MvcTestingClient()  { return _testServerSetup.TestServer.MvcTestingClient(); }
+            protected ExecutorStubAsync     ExecutorStub        => _excecutorStub.Stub;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _excecutorStub = new SetupExecutorStub(Registry.ExecutorAsync, e => Registry.ExecutorAsync = e);
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                using (_excecutorStub) { }
+            }
+        }
+
+        [TestFixture]
+        public abstract class LogicTest
+        {
+            private SetupMemoryLogic _memoryLogic;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _memoryLogic = new SetupMemoryLogic();
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                using (_memoryLogic) { }
+            }
+        }
+
+        public class SetupMemoryLogic : IDisposable
+        {
+            private Func<DateTime>      _previousNow;
+            private INhSqlRepository    _previousRepository;
+
+            public SetupMemoryLogic()
+            {
+                _previousNow = Registry.UtcNow;
+                _previousRepository = Registry.Repository.Value;
+                MemoryRepository = new NhSqlMemoryRepository(MemoryConstraints);
+                Registry.Repository.Value = MemoryRepository;
+            }
+
+            public NhSqlMemoryRepository MemoryRepository { get; private set; }
+
+            public void Dispose()
+            {
+                Registry.Repository.Value = _previousRepository;
+                Registry.UtcNow = _previousNow;
+            }
+        }
+
+        public class DbTxTest : NhSqlTxTest
+        {
+            public DbTxTest() : base(_sessionFactory.Value) { }
+        }
+
+        public class TestStartup : AbstractTestStartup { }
+    }
+}
