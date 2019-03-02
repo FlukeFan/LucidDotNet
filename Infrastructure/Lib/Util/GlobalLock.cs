206502e7c97dc18126105e34e64aefc290244cfc
Richard Brown <fluke_fan@hotmail.com>
2019-03-02 16:43:22 +0000

    Added default timeout to global lock.

--- Infrastructure/Lib/Util/GlobalLock.cs
+++ Infrastructure/Lib/Util/GlobalLock.cs
@@ -7,12 +7,14 @@ namespace Lucid.Infrastructure.Lib.Util
     {
         private Semaphore _semaphore;
 
-        public GlobalLock(string identifier)
+        public GlobalLock(string identifier) : this(identifier, TimeSpan.FromMinutes(3)) { }
+
+        public GlobalLock(string identifier, TimeSpan timeout)
         {
             bool createdNew_notUsed;
             var semaphoreName = $"Global\\{identifier}";
             _semaphore = new Semaphore(1, 1, semaphoreName, out createdNew_notUsed);
-            _semaphore.WaitOne();
+            _semaphore.WaitOne(timeout);
         }
 
         public void Dispose()
