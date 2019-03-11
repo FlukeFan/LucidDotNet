63234607a988899f0f84bedb01c3b201d2da84b7
Richard Brown <fluke_fan@hotmail.com>
2019-03-11 15:35:58 +0000

    Added tests.contract project to specify agreements.

---
+++ Modules/AppFactory/Design/Contract/FindAvailableBlueprints.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Lucid.Infrastructure.Lib.Facade;
+
+namespace Lucid.Modules.AppFactory.Design.Contract
+{
+    public class FindAvailableBlueprints : IQuery<List<BlueprintDto>>
+    {
+        public int UserId;
+    }
+}
---
+++ Modules/AppFactory/Design/Tests.Contract/ContractAgreements.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Lucid.Infrastructure.Lib.Testing.Execution;
+using Lucid.Modules.AppFactory.Design.Contract;
+
+namespace Lucid.Modules.AppFactory.Design.Tests.Contract
+{
+    public class ContractAgreements
+    {
+        public static IAgreement<FindAvailableBlueprints, List<BlueprintDto>> FindAvailableBlueprints =
+            AgreementBuilder.For(() =>
+                new FindAvailableBlueprints
+                {
+                    UserId = Defaults.UserId,
+                })
+            .Result(() =>
+                new List<BlueprintDto>
+                {
+                    new BlueprintDto { Id = 101, Name = "Blueprint1" },
+                    new BlueprintDto { Id = 102, Name = "Blueprint2" },
+                });
+    }
+}
+
