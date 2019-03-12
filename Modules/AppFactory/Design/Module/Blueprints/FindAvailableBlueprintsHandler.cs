541543861775ece811d91aaa8a99179e1a23d949
Richard Brown <fluke_fan@hotmail.com>
2019-03-12 11:39:08 +0000

    Changed query to be async using LINQ.

--- Modules/AppFactory/Design/Module/Blueprints/FindAvailableBlueprintsHandler.cs
+++ Modules/AppFactory/Design/Module/Blueprints/FindAvailableBlueprintsHandler.cs
@@ -3,17 +3,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Lucid.Infrastructure.Lib.Facade;
 using Lucid.Modules.AppFactory.Design.Contract;
+using Reposify;
 
 namespace Lucid.Modules.AppFactory.Design.Blueprints
 {
-    public class FindAvailableBlueprintsHandler : IHandleQueryAsync<FindAvailableBlueprints, List<BlueprintDto>>
+    public class FindAvailableBlueprintsHandler : IHandleQueryAsync<FindAvailableBlueprints, List<BlueprintDto>>, IDbLinq<Blueprint>
     {
-        public Task<List<BlueprintDto>> Find(FindAvailableBlueprints query)
+        public async Task<List<BlueprintDto>> Find(FindAvailableBlueprints query)
         {
-            return Task.FromResult(
-                Registry.Repository.Value.Query<Blueprint>()
-                    .Select(bp => bp.ToDto())
-                    .ToList());
+            var blueprintList = await Registry.Repository.Value.ListAsync(this);
+            return blueprintList.Select(bp => bp.ToDto()).ToList();
+        }
+
+        public IQueryable<Blueprint> Prepare(IQueryable<Blueprint> queryable)
+        {
+            return queryable;
         }
     }
 }
