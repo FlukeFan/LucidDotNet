6f05af76b5ad792921031e8c46cdae09a92f805a
Richard Brown <fluke_fan@hotmail.com>
2019-06-05 07:00:39 +0100

    Initial placeholder for Start Manufacturing Cycle.

---
+++ Modules/AppFactory/Manufacturing/Module/Domain/Cycles/StartCycleCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lucid.Lib.Facade;
+
+namespace Lucid.Modules.AppFactory.Manufacturing.Domain.Cycles
+{
+    public class StartCycleCommand : CommandAsync<int>
+    {
+        public int  BlueprintId { get; set; }
+        public int  Quantity    { get; set; }
+
+        public override Task<int> ExecuteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Modules/AppFactory/Manufacturing/Tests/Pages/CyclesTests.cs
+++ Modules/AppFactory/Manufacturing/Tests/Pages/CyclesTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Lucid.Modules.AppFactory.Manufacturing.Domain.Cycles;
 using NUnit.Framework;
 
 namespace Lucid.Modules.AppFactory.Manufacturing.Tests.Pages
@@ -13,7 +14,25 @@ namespace Lucid.Modules.AppFactory.Manufacturing.Tests.Pages
             var response = await Client().GetAsync("/appFactory/manufacturing/cycles");
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-            response.Text.Should().Contain("Hello from Cycles page");
+            response.Doc.Find("a[data-modal-dialog=true]").TextContent.Should().Be("Start Cycle");
+        }
+
+        [Test]
+        public async Task Can_StartCycle()
+        {
+            ExecutorStub.StubResult<StartCycleCommand>(123);
+
+            var client = Client();
+
+            var getResponse = await client
+                .GetAsync("/appFactory/manufacturing/cycles/start");
+
+            //var response = await getResponse.Form<StartCycleCommand>()
+            //    .Submit();
+
+            getResponse.Should().NotBeNull();
+
+            //ExecutorStub.VerifySingleExecuted(Agreements.Start);
         }
     }
 }
